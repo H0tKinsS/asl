b3723ad226cfcd2ccf30afec237b37ca276c7531
H0tKinsS <68612148+H0tKinsS@users.noreply.github.com>
2025-07-04 21:34:44 +0200

    Update nat.sh

--- nat.sh
+++ nat.sh
@@ -1,4 +1,7 @@
-iptables -t nat -A POSTROUTING -j MASQUERADE -o eth2 -s /24
+iptables -t nat -A POSTROUTING -j MASQUERADE -o eth2 -s 10.21.130.0/24
 
-iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 1001 --to 192.168.130.101:22
-iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 2001 --to 192.168.130.102:22
+iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 1001 --to 10.21.130.11:22
+iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 2002 --to 10.21.130.12:22
+
+iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 8016 --to 10.21.130.11:80
+iptables -t nat -I PREROUTING -j DNAT -p tcp -d 192.168.76.130 --dport 8006 --to 10.21.130.12:80
